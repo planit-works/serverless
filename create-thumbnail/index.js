48fc3875a59894dfae572dbbefeb757ff6be064f
KeonU Kim <anythingthannothing@gmail.com>
2023-02-12 23:11:51 +0900

    ✨ feat: s3 버킷 업로드 시 썸네일 이미지 생성 함수 추가
    추후 버킷명 및 파일명 등 fe와 협의 필요

---
+++ create-thumbnail/index.js
@@ -0,0 +1,73 @@
+import AWS from 'aws-sdk';
+import util from 'util';
+import sharp from 'sharp';
+
+const s3 = new AWS.S3();
+
+export const handler = async (event, context, callback) => {
+  console.log('이벤트 옵션 내역: ', util.inspect(event, { depth: 5 }));
+  const srcBucket = event.Records[0].s3.bucket.name;
+  // Object key may have spaces or unicode non-ASCII characters.
+  const srcKey = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, ' '),
+  );
+  const dstBucket = `${srcBucket}-resized`;
+  const dstKey = `${srcKey}-resized`;
+
+  //   const typeMatch = srcKey.match(/\.([^.]*)$/);
+  //   if (!typeMatch) {
+  //     console.log('Could not determine the image type.');
+  //     return;
+  //   }
+
+  //   const imageType = typeMatch[1].toLowerCase();
+  //   if (imageType != 'jpg' && imageType != 'png') {
+  //     console.log(`Unsupported image type: ${imageType}`);
+  //     return;
+  //   }
+
+  try {
+    const params = {
+      Bucket: srcBucket,
+      Key: srcKey,
+    };
+    var origimage = await s3.getObject(params).promise();
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  const width = 200;
+
+  try {
+    var buffer = await sharp(origimage.Body).resize(width).toBuffer();
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  try {
+    const destparams = {
+      Bucket: dstBucket,
+      Key: dstKey,
+      Body: buffer,
+      ContentType: 'image',
+    };
+
+    const putResult = await s3.putObject(destparams).promise();
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(
+    'Successfully resized ' +
+      srcBucket +
+      '/' +
+      srcKey +
+      ' and uploaded to ' +
+      dstBucket +
+      '/' +
+      dstKey,
+  );
+};
